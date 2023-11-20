03dbeb2eb016f3e08d3bcc2ac695f33d9d5e0fc8
setongee <setongee@gmail.com>
2023-11-20 07:13:32 +0100

    done

---
+++ src/Privacy.jsx
@@ -0,0 +1,197 @@
+import React from 'react';
+import './privacy.scss'
+
+const Privacy = () => {
+
+    return (
+
+        <div className="privacy">
+
+            <div className="headPlace">
+
+                <h1>Privacy Policy</h1>
+                <p>Updated and Reviewed March 10, 2022</p>
+
+            </div>
+
+            <div className="contentPrivacy">
+
+
+                    <div className="topic">
+                        1. Introduction
+                    </div>
+
+                    Hi! We've created this privacy policy to explain how we collect, use, disclose and protect your information; including any nonpublic personal information.
+                    This policy applies to information we collect when you use our website and mobile application (collectively, "Services") or otherwise interact with us as described below. We may change this privacy policy overtime. If and/or when we make changes, we will notify you by revising the date at the top of this policy, and in some cases, we may provide you with additional notice (such as by adding a statement to the homepages of our website or mobile application or by sending you an email notification). {<br></br>} {<br></br>}
+                    We encourage you to review the privacy policy whenever you interact with us to stay informed about our information practices and the ways you can help protect your privacy. This privacy policy applies to the Services provided by Feather (“we” or “us”).
+
+                    {<br></br>}{<br></br>}
+
+
+                    <div className="topic">
+                    2. Collection of Information
+                    </div>
+
+                    <strong>2.1</strong> Categories of Non-public Personal Information  {<br></br>}
+                    We collect nonpublic personal information you provide to us when you register for a Feather wallet or when you communicate with us about your Feather account. The types of information we may collect include your name, address, phone number, email address, bank account information, debit card details, username, password.
+
+                    {<br></br>} {<br></br>}
+
+                    <strong>2.2</strong> Information About Your Transactions with Us {<br></br>}
+                    As you use your Feather account to perform financial transactions, we collect nonpublic personal information in connection with each transaction, including transaction time, Naira amount and merchant details. We may also collect photos, memos or other information that you attach to your transactions.
+
+                    {<br></br>} {<br></br>}
+
+                    <strong>2.3</strong> Information from Other Sources {<br></br>}
+                    When you create a Feather account, we may collect information, including non-public personal information about you from non-affiliated third-party service providers in order to verify your identity and for fraud prevention, including your prior addresses and names.
+
+                    {<br></br>} {<br></br>}
+
+                    <strong>2.4</strong> Other Information we collect {<br></br>}
+                    We may collect information you provide to us, such as your name, email address and any other information you choose to provide. For example, we may collect such information if you request an invite to join Feather or if you enter into any contest or promotion.
+
+                    {<br></br>} {<br></br>}
+
+                    <strong>2.5</strong> Information We Collect Automatically from our Services {<br></br>}
+                    When you access or use our Services, we automatically collect information about you as follows :
+
+                    {<br></br>}{<br></br>}
+
+                    Location Information : We may collect your location information from your mobile device with your prior consent. Also, we have incorporated Google Maps into our Services, and you may consent to the collection of location information by Google Maps in connection with your use of this maps service. Any information collected via your use of Google Maps will be transmitted directly to Google and is not collected by us. Please refer to Google’s privacy policy for details about their collection, use and sharing of this information. For information about how to disable the collection of location information from your device, please see “Your Choices” below. 
+                    
+                    {<br></br>}{<br></br>}
+
+                    Information Collected by Cookies and Other Tracking Technologies: We use various technologies to collect information on our Services and other websites, and this may include sending cookies to your computer or mobile device. Cookies are small data files stored on your hard drive or in device memory that help us to improve our Services and your experience, see which areas and features of our Services are popular and count visits. We may also collect information using web beacons (also known as "tracking pixels"). Web beacons are electronic images that may be used in our Services or emails and help deliver cookies, count visits, understand usage and campaign effectiveness and determine whether an email has been opened and acted upon.
+
+                    {<br></br>}{<br></br>}
+
+                    The app uses third party services that may collect information used to identify you. Links to privacy policy of third-party service providers used by the app : {<br></br>}{<br></br>}
+
+                    <strong>- Google Play Services</strong>{<br></br>}{<br></br>}
+                    <strong>- Google analytics</strong>{<br></br>}{<br></br>}
+                    <strong>- AdMob</strong>{<br></br>}{<br></br>}
+                    <strong>- Firebase Analytics</strong>{<br></br>}{<br></br>}
+                    <strong>- Fabric</strong>{<br></br>}{<br></br>}
+                    <strong>- Crashlytics</strong>{<br></br>}{<br></br>}
+                    <strong>- Adjust</strong>{<br></br>}{<br></br>}
+                    <strong>- Facebook</strong>{<br></br>}{<br></br>}
+
+
+
+
+                    <strong>2.6</strong> Information we receive from other sources {<br></br>}
+                    We work closely with third parties (including financial service providers, security agencies, etc.). We will notify you when we receive information about you from them and the purposes for which we intend to use that information.
+
+                    {<br></br>}{<br></br>}
+
+                    <div className="topic"> 3. Use of Information </div>
+
+                    We may use your information, including non-public personal information as follows: {<br></br>} {<br></br>}
+
+                    1. Provide, maintain and improve our Services {<br></br>} {<br></br>}
+
+                    2. Provide and deliver the products and services you request, process transactions and send you related information, including confirmations.
+                    Verify your identity and prevent fraud. {<br></br>} {<br></br>}
+
+                    3. Send you technical notices, updates, security alerts and support and administrative messages. {<br></br>} {<br></br>}
+
+                    4. Respond to your comments, questions and requests and provide customer service. {<br></br>} {<br></br>}
+
+                    5. Communicate with you about products, services, offers, promotions, rewards, and events offered by Feather and others, and provide news and information we think will be of interest to you. {<br></br>} {<br></br>}
+
+                    6. Monitor and analyze trends, usage and activities in connection with our Services. {<br></br>} {<br></br>}
+
+                    7. Personalize and improve the Services and provide advertisements, content or features that match user profiles or interests.
+                    Process and deliver contest or promotion entries and rewards. {<br></br>} {<br></br>}
+
+                    8. Link or combine with information we get from others to help understand your needs and provide you with better service.
+                    Carry out any other purpose for which the information was collected. {<br></br>} {<br></br>}
+
+                    9. Feather is based in Nigeria and the information we collect is governed by the Nigerian law. By accessing or using the Services or otherwise providing information to us, you consent to the processing and transfer of information in and to Nigeria and other countries. {<br></br>} {<br></br>}
+
+                    <div className="topic">
+                    4. Disclosure of Information
+                    </div>
+                    
+                    We may disclose any information we collect about current and former customers, including nonpublic personal information, to affiliates and non-affiliated third parties as follows: {<br></br>} {<br></br>}
+
+                    With financial service providers, including the financial institutions identified in your cardholder bank agreement that provide banking services in connection with your Feather account. {<br></br>} {<br></br>}
+
+                    With another user, when you sign up for Feather's services via a referral link. The user that referred you may receive information indicating that you have enrolled with Feather. You may avoid this sharing by not using a referral link to enroll.{<br></br>} {<br></br>}
+
+                    With non-financial companies, such as email service providers that perform marketing services on our behalf, and fraud prevention service providers that use the information to provide services to Feather and other companies.{<br></br>} {<br></br>}
+
+                    With a non-affiliated third-party to access and transmit your personal and financial information from a relevant financial institution. You grant the third-party the right, power, and authority to access and transmit this information. according to terms of their privacy policy.{<br></br>} {<br></br>}
+
+                    With other nonaffiliated companies for our everyday business purposes, such as to process transactions, maintain accounts, respond to court orders and legal investigations or report to credit bureaus. For example, in connection with our everyday business purposes, we may share information about you as follows:{<br></br>} {<br></br>}
+
+                    In response to a request for information, if we are required by, or we believe disclosure is in accordance with, any applicable law, regulation or legal process. {<br></br>} {<br></br>}
+
+                    With relevant law enforcement officials or other third parties, such as investigators or auditors, if we believe it is appropriate to investigate fraud.
+                    If we believe your actions are inconsistent with the spirit or language of our user agreements or policies, or to protect the rights, property and safety of Feather or others. {<br></br>} {<br></br>}
+
+                    In connection with, or during negotiations of, any merger, sale of Feather's assets, financing or acquisition of all or a portion of our business to another company; and
+                    With your consent or at your direction, including if we notify you that the information you provide will be shared in a particular manner and you provide such information. {<br></br>} {<br></br>}
+
+                    We may also share aggregated or de-identified Information, which cannot reasonably be used to identify you. For example, we may share transaction zip codes with third parties to improve our accuracy in geo-locating transactions and to improve the clarity of transaction descriptions. {<br></br>} {<br></br>}
+
+                    We will usually not share your personal information with other third parties without your consent. Where we need to transfer your data to another country, we shall ensure that such country to which the data is being transferred shall have adequate data protection law. We will seek your consent where we need to send your data to a country without an adequate data protection law. {<br></br>} {<br></br>}
+
+                    <div className="topic">
+                        5. Your Choices
+                    </div>
+
+                    <strong>5.1</strong> Account Information {<br></br>}
+                    You may edit your account profile information by logging into your account via our Services. If you wish to delete or deactivate your online account, please contact us, but note that we may retain certain information as required by law or for legitimate business purposes.
+
+                    {<br></br>}{<br></br>}
+
+                    <strong>5.2</strong> Promotional Communications {<br></br>}
+                    You may opt out of receiving promotional emails and text messages from Feather by following the instructions in those messages. Please note that if you opt out, we may still send you transactional or relationship messages, such as those about your account or our ongoing business relations.
+
+                    {<br></br>}{<br></br>}
+
+                    <strong>5.3</strong> Location Information {<br></br>}
+                    If you initially consent to the collection of location information by Feather or Google Maps within our mobile application, you may be able to subsequently stop this collection through your device operating system settings or through your online account settings. If either of these opt-out options are not available to you, you may also disable location information by following the standard uninstall process to remove our mobile application from your device.
+
+                    {<br></br>}{<br></br>}
+
+                    <strong>5.4</strong> Cookies {<br></br>}
+                    Most web browsers are set to accept cookies by default. If you prefer, you can usually choose to set your browser to remove or reject browser cookies. Please note that if you choose to remove or reject cookies, this could affect the availability and functionality of our Services.
+
+                    {<br></br>}{<br></br>}
+
+                    <strong>5.5</strong> Social Sharing Features {<br></br>}
+                    Our Services include social sharing features for common social networks to let users share content. These features are served from third-party services that may serve their own cookies. The use of such features enables the sharing of information with your friends or the public, depending on the settings you establish with the entity that provides the social sharing feature. For more information about the purpose and scope of data collection and processing in connection with social sharing features, please visit the privacy policies of the entities that provide these features.
+
+                    {<br></br>}{<br></br>}
+                    
+                    <strong>5.6</strong> Analytics and Advertising Services Provided by Others {<br></br>}
+                    We may allow others to provide analytics services on our behalf and to serve advertisements on our behalf across the Internet. These entities may use cookies, web beacons and other technologies to collect information about your use of the Services and other websites, including your IP address, web browser, pages viewed, time spent on pages, links clicked and conversion information. This information may be used by Feather and others, to, among other things, analyze and track data, determine the popularity of certain content, deliver advertising targeted to your interests and better understand your online activity.
+
+                    {<br></br>}{<br></br>}
+
+                    <div className="topic">6. Security</div>
+                    
+                    Feather takes reasonable measures to help protect all information about you from loss, theft, misuse and unauthorized access, disclosure, alteration and destruction. Additionally, Feather implements policies designed to protect the confidentiality and security of your nonpublic personal information, including a privacy protection policy. Feather limits access to your nonpublic personal information to employees that have a business reason to know such information, and implement security practices and procedures designed to protect the confidentiality and security of such information and prohibit unlawful disclosure of such information in accordance with its policies. {<br></br>}{<br></br>}
+                    Where you have chosen a password that allows you to access certain parts of the website and mobile application, you are responsible for keeping this password confidential. We advise you not to share your password with anyone. We have also taken measures to comply with provision of security facilities for the protection of your nonpublic personal information through the setup of firewalls, limited access to specified authorized individuals, encryption and continuous capacity building for relevant personnel. We therefore have digital and physical security measures to limit and eliminate possibilities of data privacy breach incidents.{<br></br>}{<br></br>}
+                    Although we use appropriate security measures once we have received your personal information, the transmission of data over the internet (including by email) is never completely secure. We endeavour to protect personal information, but we cannot guarantee the security of data transmitted to us or by us. We will notify you and any applicable regulator of a breach where we are legally required to do so.{<br></br>}{<br></br>}
+
+                    <div className="topic">7. Where we store your Data</div>
+                    The data that we collect from you will be transferred to and stored at a destination outside Nigeria. By submitting your personal data, you agree to this transfer, storing or processing. We will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this privacy policy. All information you provide to us is stored on our secure cloud storage solution.{<br></br>}{<br></br>}
+                    Your personal information will not be retained for longer than is necessary for the purposes for which it was processed.{<br></br>}{<br></br>}
+
+                    <div className="topic">8. How long we keep your personal information</div>
+                    We will hold your personal information on Feather’s systems for as long as is necessary to fulfil the purpose for which it was collected or to comply with legal, regulatory or internal policy requirements.{<br></br>}{<br></br>}
+                    Contact Us
+                    If you have any questions about this Privacy Policy, please contact us at: <a href = "mailto:support@feather.africa" > support@feather.africa </a>
+
+
+
+                    </div>
+
+        </div>
+    );
+}
+
+export default Privacy;
--- src/routes/router.js
+++ src/routes/router.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Routes, Route, useNavigate} from 'react-router-dom'
 import HomePage from '../pages/homepage/homepage';
 import Support from '../pages/support/support';
+import Privacy from '../Privacy';
 
 const RouterClass = () => {
 
@@ -13,6 +14,7 @@ const RouterClass = () => {
 
                 <Route index element = {<HomePage/>} />
                 <Route path = 'support' element = {<Support/>} />
+                <Route path = 'privacy' element = {<Privacy/>} />
 
                 <Route path="*" element = {<h1>Page not found</h1>} />
 
